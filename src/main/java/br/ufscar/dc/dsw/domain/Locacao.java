74ba5630e6d9793931581587c89589032d03b25f
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2023-08-28 12:50:17 -0300

    Organizando codigos da Locacao REST

--- src/main/java/br/ufscar/dc/dsw/domain/Locacao.java
+++ src/main/java/br/ufscar/dc/dsw/domain/Locacao.java
@@ -1,20 +1,12 @@
 package br.ufscar.dc.dsw.domain;
 
-import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
